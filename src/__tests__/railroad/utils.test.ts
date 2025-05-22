844951d961ffcba2d4d2c50dfec35ab7cc20f0dc
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2025-05-22 20:47:02 +0100

    standardize tests for the utils methods

--- src/__tests__/railroad/utils.test.ts
+++ src/__tests__/railroad/utils.test.ts
@@ -2,15 +2,17 @@ import { escape } from '../../railroad/utils';
 
 describe('Railroad / utils / escape', () => {
 
-    test('leaves strings without special characters untouched', () => {
+    it('leaves strings without special characters untouched', () => {
         const original = 'hello world';
         const escaped = escape(original);
+
         expect(escaped).toBe(original);
     });
 
-    test('replaces special characters', () => {
+    it('replaces special characters', () => {
         const original = '<hello *world*>';
         const escaped = escape(original);
+
         expect(escaped).toBe('&#60;hello &#42;world&#42;&#62;');
     });
 
