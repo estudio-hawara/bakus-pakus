fcdeac15a7273a3ea3290ee6fc5b21eaab21752d
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2025-05-23 14:30:28 +0100

    standardize the tokenizer tests

--- src/__tests__/tokenizer.test.ts
+++ src/__tests__/tokenizer.test.ts
@@ -2,76 +2,164 @@ import { Token, Tokenizer } from '../tokenizer';
 
 describe('Token', () => {
 
-    test('is a class', () => {
-        expect(typeof Token).toBe('function');
+    describe('Constructor', () => {
+
+        it('is a class', () => {
+            expect(typeof Token).toBe('function');
+        });
+
     });
 
-    test('token type and its value', () => {
-        const type = 'identifier';
-        const value = 'Left hand side';
-        const token = new Token(type, value);
+    describe('Getter: type', () => {
 
-        expect(token.type).toBe(type);
-        expect(token.value).toBe(value);
+        it('token type and its value', () => {
+            const type = 'identifier';
+            const value = 'Left hand side';
+            const token = new Token(type, value);
+
+            expect(token.type).toBe(type);
+        });
+
+    });
+
+    describe('Getter: value', () => {
+
+        it('token type and its value', () => {
+            const type = 'identifier';
+            const value = 'Left hand side';
+            const token = new Token(type, value);
+
+            expect(token.value).toBe(value);
+        });
+
     });
 
 });
 
 describe('Tokenizer', () => {
 
-    test('is a class', () => {
-        expect(typeof Tokenizer).toBe('function');
+    describe('Constructor', () => {
+
+        it('is a class', () => {
+            expect(typeof Tokenizer).toBe('function');
+        });
+
     });
 
-    test('exposes properties of it\'s reader', () => {
-        const tokenizer = new Tokenizer;
+    describe('Getter: position', () => {
 
-        expect(tokenizer.position).toBe(0);
-        expect(tokenizer.line).toBe(1);
-        expect(tokenizer.column).toBe(1);
+        it('\'s current position can be accessed', () => {
+            const tokenizer = new Tokenizer;
+
+            expect(tokenizer.position).toBe(0);
+        });
+
     });
 
-    test('can read text', () => {
-        const tokenizer = new Tokenizer;
-        const source = 'binary = "0" | "1";';
-        tokenizer.read(source);
+    describe('Getter: line', () => {
 
-        expect(tokenizer.source).toBe(source);
+        it('\'s current line can be accessed', () => {
+            const tokenizer = new Tokenizer;
+
+            expect(tokenizer.line).toBe(1);
+        });
+
     });
 
-    test('returns null when it reaches its end', () => {
-        const tokenizer = new Tokenizer;
-        tokenizer.read('');
+    describe('Getter: column', () => {
 
-        expect(tokenizer.getNextToken()).toBeNull();
+        it('\'s current column can be accessed', () => {
+            const tokenizer = new Tokenizer;
+
+            expect(tokenizer.column).toBe(1);
+        });
+
     });
 
-    test('process ebnf tokens', () => {
-        const tokenizer = new Tokenizer;
-        const source = 'binary = "0" | "1";';
-        tokenizer.read(source);
-        const token = tokenizer.getNextToken();
+    describe('Getter: finished', () => {
 
-        expect(token?.type).toBe('identifier');
-        expect(token?.value).toBe('binary');
+        it('\'s finished flag can be accessed', () => {
+            const tokenizer = new Tokenizer;
+
+            expect(tokenizer.finished).toBeTruthy();
+        });
+
     });
 
-    test('throws an exception when an unexpected token is found', () => {
-        const tokenizer = new Tokenizer;
-        const source = '!!!';
-        tokenizer.read(source);
+    describe('Getter: source', () => {
 
-        expect(() => { tokenizer.getNextToken(); }).toThrow(SyntaxError);
+        it('\'s source text can be accessed', () => {
+            const tokenizer = new Tokenizer;
+
+            expect(tokenizer.source).toBe('');
+        });
+
     });
 
-    test('skips ignorable tokens (like whitespaces)', () => {
-        const tokenizer = new Tokenizer;
-        const source = '\n binary = "0" | "1";';
-        tokenizer.read(source);
-        const token = tokenizer.getNextToken();
+    describe('Method: read', () => {
 
-        expect(token?.type).toBe('identifier');
-        expect(token?.value).toBe('binary');
+        it('can read text', () => {
+            const tokenizer = new Tokenizer;
+            const source = 'binary = "0" | "1";';
+            tokenizer.read(source);
+
+            expect(tokenizer.source).toBe(source);
+        });
+
+    });
+
+    describe('Method: reset', () => {
+
+        it('can be reset', () => {
+            const tokenizer = new Tokenizer;
+            const source = 'binary = "0" | "1";';
+            tokenizer.read(source);
+            tokenizer.reset();
+
+            expect(tokenizer.position).toBe(0);
+            expect(tokenizer.line).toBe(1);
+            expect(tokenizer.column).toBe(1);
+        });
+
+    });
+
+    describe('Method: getNextToken', () => {
+
+        it('returns null when it reaches its end', () => {
+            const tokenizer = new Tokenizer;
+            tokenizer.read('');
+
+            expect(tokenizer.getNextToken()).toBeNull();
+        });
+
+        it('process ebnf tokens', () => {
+            const tokenizer = new Tokenizer;
+            const source = 'binary = "0" | "1";';
+            tokenizer.read(source);
+            const token = tokenizer.getNextToken();
+
+            expect(token?.type).toBe('identifier');
+            expect(token?.value).toBe('binary');
+        });
+
+        it('throws an exception when an unexpected token is found', () => {
+            const tokenizer = new Tokenizer;
+            const source = '!!!';
+            tokenizer.read(source);
+
+            expect(() => { tokenizer.getNextToken(); }).toThrow(SyntaxError);
+        });
+
+        it('skips ignorable tokens (like whitespaces)', () => {
+            const tokenizer = new Tokenizer;
+            const source = '\n binary = "0" | "1";';
+            tokenizer.read(source);
+            const token = tokenizer.getNextToken();
+
+            expect(token?.type).toBe('identifier');
+            expect(token?.value).toBe('binary');
+        });
+
     });
 
 });
